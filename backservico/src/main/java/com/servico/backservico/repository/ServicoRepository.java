3393c25d758fa9bd0475c4b54cebfd25b2473aec
Filipe <filipeandrade000@gmail.com>
2022-12-27 00:29:39 -0300

    ServicoService e ServicoRepository

---
+++ backservico/src/main/java/com/servico/backservico/repository/ServicoRepository.java
@@ -0,0 +1,14 @@
+package com.servico.backservico.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.servico.backservico.entity.Servico;
+// Tabela servico e atributo long. ID
+public interface ServicoRepository extends JpaRepository<Servico,Long>{
+    
+    @Query("select s from Servico s where s.valorPago <> null and valorPago >0")
+    List<Servico> buscarServicosPagamentoPendente();
+}
---
+++ backservico/src/main/java/com/servico/backservico/service/ServicoService.java
@@ -0,0 +1,50 @@
+package com.servico.backservico.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.servico.backservico.entity.Servico;
+import com.servico.backservico.repository.ServicoRepository;
+
+@Service
+public class ServicoService {
+    
+    @Autowired //injeção de dependencias, sem instanciação manuais
+    private ServicoRepository servicoRepository;
+
+    public List<Servico> buscarTodos(){
+
+        return servicoRepository.findAll();
+    }
+
+    public List<Servico> buscarServicosPagamentoPendente(){
+        return servicoRepository.buscarServicosPagamentoPendente();
+    }
+
+    public Servico inserir(Servico servico){
+       //return servicoRepository.save(servico);
+
+       if(servico.getValorPago() == null || servico.getValorPago() == 0 || servico.getDataPagamento() == null){
+        servico.setStatus("pendente");
+       }else{
+        servico.setStatus("realizado");
+       }
+       Servico servicoBanco = servicoRepository.saveAndFlush(servico);
+       return servicoBanco;
+    }
+
+    public Servico alterar(Servico servico){
+        if(servico.getValorPago() != null &&  servico.getValorPago() > 0 && servico.getDataPagamento() != null){
+            servico.setStatus("realizado");
+           }
+
+        return servicoRepository.saveAndFlush(servico);
+    }
+    
+    public void excluir(Long id){
+        Servico servico = servicoRepository.findById(id).get();
+        servicoRepository.delete(servico);
+    }
+}
