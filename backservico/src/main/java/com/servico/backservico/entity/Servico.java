62cd27aa9b05ebef408bb0158e7f479b8ea0a5e1
Filipe <filipeandrade000@gmail.com>
2022-12-27 00:07:17 -0300

    Entity

---
+++ backservico/src/main/java/com/servico/backservico/entity/Servico.java
@@ -0,0 +1,34 @@
+package com.servico.backservico.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity 
+@Table(name = "servico") //cria tabela servico
+@Data //get e set
+public class Servico {
+    
+    @Id //chave primária
+    @GeneratedValue(strategy = GenerationType.AUTO) //gera automaticamente
+    private long id;
+    private String nomeCliente;
+    @Temporal(TemporalType.DATE) //formato
+    private Date dataInicio = new Date();
+    @Temporal(TemporalType.DATE)
+    private Date dataTermino;
+    private String descricaoServico;
+    private Double valorServico;
+    private Double valorPago;
+    @Temporal(TemporalType.DATE)
+    private Date dataPagamento;
+    private String status; //Pendente, Realizado ou Cancelado
+
+}
