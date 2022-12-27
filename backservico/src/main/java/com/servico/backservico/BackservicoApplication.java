faa0d389acfe6f456291688d45e0d4faae62f0e2
Filipe <filipeandrade000@gmail.com>
2022-12-26 23:06:03 -0300

    Primeiro commit

---
+++ backservico/src/main/java/com/servico/backservico/BackservicoApplication.java
@@ -0,0 +1,13 @@
+package com.servico.backservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackservicoApplication.class, args);
+	}
+
+}
---
+++ backservico/src/test/java/com/servico/backservico/BackservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servico.backservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
