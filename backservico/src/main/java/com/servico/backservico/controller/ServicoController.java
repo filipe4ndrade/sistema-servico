082601ef6c898a4f858ea77f3332eb944b39d0bd
Filipe <filipeandrade000@gmail.com>
2022-12-27 21:23:03 -0300

    Front completo

--- backservico/src/main/java/com/servico/backservico/controller/ServicoController.java
+++ backservico/src/main/java/com/servico/backservico/controller/ServicoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,37 +26,44 @@ public class ServicoController {
     private ServicoService servicoService;
 
     @GetMapping("/") //rota do url
+    @CrossOrigin("http://localhost:3000")
     public List<Servico> buscarTodos(){
         return servicoService.buscarTodos();
     }
 
     @GetMapping("/pagamentoPendente")
+    @CrossOrigin("http://localhost:3000")
     public List<Servico> buscarServicosPagamentoPendente(){
         return servicoService.buscarServicosPagamentoPendente();
     }
 
     @GetMapping("/cancelados")
+    @CrossOrigin("http://localhost:3000")
     public List<Servico> buscarServicosCancelados(){
         return servicoService.buscarServicosCancelados();
     }
 
     @PostMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Servico inserir(@RequestBody Servico servico){
         return servicoService.inserir(servico);
     }
 
     @PostMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> cancelar(@PathVariable("id") Long id){
         servicoService.cancelarServico(id);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Servico alterar(@RequestBody Servico servico){
         return servicoService.alterar(servico);
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
         servicoService.excluir(id);
         return ResponseEntity.ok().build();
--- frontservico/src/servico/Servico.jsx
+++ frontservico/src/servico/Servico.jsx
@@ -1,19 +1,79 @@
 import React, {useEffect, useState} from 'react';
 import './Servico.css';
+import axios from 'axios';
 
 function Servico() {
 
-    const [servico, setServico] = useState({nomeCliente:'',dataInicio:'',dataTermino:'',descricaoServico:'',valorPago:'',dataPagamento:''});
-    const [services, setServicos] = useState([]);
+    const [servico, setServico] = useState({nomeCliente:'',dataInicio:'',dataTermino:'',descricaoServico:'',valorServico:'',valorPago:'',dataPagamento:''});
+     
+    const [servicos, setServicos] = useState([]);
+    const [atualizar,setAtualizar] = useState();
+
+     
+    useEffect(()=>{
+      buscarTodos();
+   },[atualizar]);
+
+   
 
     //função alterar
     function handleChange(event){
         setServico({...servico,[event.target.name]:event.target.value})
     }
 
+    function buscarTodos(){
+      axios.get("http://localhost:8080/api/servico/").then(result=>{
+       setServicos(result.data);
+    
+      });
+    }
+
+    function buscarPagamentosPendentes(){
+      axios.get("http://localhost:8080/api/servico/pagamentoPendente").then(result=>{
+        setServicos(result.data);
+     
+       });
+    }
+
+    function buscarCancelados(){
+      axios.get("http://localhost:8080/api/servico/cancelados").then(result=>{
+        setServicos(result.data);
+     
+       });
+    }
+
+
+    function limpar(){
+        setServico({nomeCliente:'',dataInicio:'',dataTermino:'',descricaoServico:'',valorServico:'',valorPago:'',dataPagamento:''});
+    }
+
     function handleSubmit(event){
         event.preventDefault(); //evita que na página ocorra refresh
+        //console.log(servico);
+        if(servico.id==undefined){
+        axios.post("http://localhost:8080/api/servico/", servico).then(result=>{
+          setAtualizar(result);
+        });
+      }else{
+        axios.put("http://localhost:8080/api/servico/", servico).then(result=>{
+          setAtualizar(result);
+      });
     }
+    limpar();
+  }
+
+  function excluir(id){
+    axios.delete("http://localhost:8080/api/servico/"+id).then(result=>{
+    setAtualizar(result);
+  });
+  }
+
+  function cancelar(id){
+    axios.post("http://localhost:8080/api/servico/"+id).then(result=>{
+    setAtualizar(result);
+  });
+  }
+
 
   return (
     <div className ="container">
@@ -22,33 +82,82 @@ function Servico() {
         <div className="col-6">
           <div>
               <label className="form-label">Nome do Cliente</label>
-              <input onChange={handleChange} value={servico.nomeCliente} name="nomeCliente" type="text" className="form-control"/>
+              <input onChange={handleChange} value={servico.nomeCliente || ''} name="nomeCliente" type="text" className="form-control"/>
           </div>
           <div>
               <label className="form-label">Data de Início</label>
-              <input onChange={handleChange} value={servico.dataInicio} name="dataInicio" type="date" className="form-control"/>
+              <input onChange={handleChange} value={servico.dataInicio || ''} name="dataInicio" type="date" className="form-control"/>
           </div>
           <div>
               <label className="form-label">Data de Término</label>
-              <input onChange={handleChange} value={servico.dataTermino} name="dataTermino" type="data" className="form-control"/>
+              <input onChange={handleChange} value={servico.dataTermino || ''} name="dataTermino" type="date" className="form-control"/>
           </div>
           <div>
               <label className="form-label">Descrição do Serviço</label>
-              <input onChange={handleChange} value={servico.descricaoServico} name="descricaoServico" type="text" className="form-control"/>
+              <input onChange={handleChange} value={servico.descricaoServico || ''} name="descricaoServico" type="text" className="form-control"/>
+          </div>
+          <div>
+              <label className="form-label">Valor do Serviço</label>
+              <input onChange={handleChange} value={servico.valorServico || ''} name="valorServico" type="number" className="form-control"/>
           </div>
           <div>
               <label className="form-label">Valor Pago</label>
-              <input onChange={handleChange} value={servico.valorPago} name="valorPago" type="number" className="form-control"/>
+              <input onChange={handleChange} value={servico.valorPago || ''} name="valorPago" type="number" className="form-control"/>
           </div>
           <div>
               <label className="form-label">Data de Pagamento</label>
-              <input value={servico.dataPagamento} name="dataPagamento" type="date" className="form-control"/>
+              <input onChange={handleChange} value={servico.dataPagamento || ''} name="dataPagamento" type="date" className="form-control"/>
           </div>
     <br/>
           <input type="submit" className="btn btn-success" value="Cadastrar"></input>
           
         </div>
       </form>
+
+      <hr/>
+      <hr/>
+
+<button onClick ={buscarTodos} type="button" class="btn btn-primary">Listar Todos</button>&nbsp;&nbsp;
+<button onClick ={buscarPagamentosPendentes} type="button" class="btn btn-secondary">Serviços com Pagamento Pendente</button>&nbsp;&nbsp;
+<button onClick ={buscarCancelados} type="button" class="btn btn-success">Serviços Cancelados</button>
+
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Descrição</th>
+      <th scope="col">Valor</th>
+      <th scope="col">Status</th>
+      <th scope="col">Opções</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      servicos.map(serv=>
+        <tr key={serv.id}>
+      <td>{serv.nomeCliente}</td>
+      <td>{serv.descricaoServico}</td>
+      <td>{serv.valorServico}</td>
+      <td>{serv.status}</td>
+      <td>
+        {serv.status!='cancelado' &&
+        <button onClick={()=>setServico(serv)} className="btn btn-primary">Alterar</button>
+        }&nbsp;&nbsp;
+        {serv.status!='cancelado' &&
+        <button onClick={()=>excluir(serv.id)} className="btn btn-danger">Excluir</button>
+        }&nbsp;&nbsp;
+        
+        <button onClick={()=>cancelar(serv.id)} className="btn btn-warning">Cancelar</button>
+
+      </td>
+    </tr>
+        
+        )
+    }
+    
+  </tbody>
+</table>
+
     </div>
   );
 }
